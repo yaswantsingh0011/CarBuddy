e0923d23fdab64ad19c4ce4f8f592f7179fdeb79
Yaswant Singh <rohitsingh24378@gmail.com>
2025-11-01 12:19:22 +0530

    Fixed all component import casing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
-import { AuthProvider } from '@/context/authContext';
+import { AuthProvider } from '@/context/AuthContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
