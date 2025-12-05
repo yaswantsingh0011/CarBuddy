c70a4bee3f48c0e269d0ae699bfce70986ef27db
Yaswant Singh <rohitsingh24378@gmail.com>
2025-12-05 11:11:25 +0530

    add features

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,17 +1,15 @@
 import { MetadataRoute } from 'next';
 
+// BASE_URL Updated to Vercel URL
 export default function robots(): MetadataRoute.Robots {
-  const BASE_URL = 'https://carbuddy.in';
+  const BASE_URL = 'https://car-buddy-new.vercel.app';
 
-  return {
-    rules: {
-      userAgent: '*', // Sabhi bots ke liye allowed hai
-      allow: '/',
-      disallow: ['/admin/', '/private/'], // In pages ko Google se chupana hai
-    },
-    sitemap: `${BASE_URL}/sitemap.xml`, // Ye line sabse zaroori hai
-  };
+  return {
+    rules: {
+      userAgent: '*', // Sabhi bots ke liye allowed hai
+      allow: '/',
+      disallow: ['/admin/', '/private/'], // In pages ko Google se chupana hai
+    },
+    sitemap: `${BASE_URL}/sitemap.xml`, // Ye line sabse zaroori hai (Vercel URL use ho raha hai)
+  };
 }
-
-
-
